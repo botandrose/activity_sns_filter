4271d632cdee3357e12ec2e9fc1471d38328f9ec
Micah Geisel <micah@botandrose.com>
2020-05-14 09:41:50 -0700

    merge most of index.js into app.js for better testability.

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const expressBasicAuth = require('express-basic-auth');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 let app = {
   express: express().use(bodyParser.text()),
 
-  set basicAuth(users) {
-    if(users && !Object.values(users).includes(undefined)) {
-      this.express.use(expressBasicAuth({
-        users: users,
-      }));
-    };
-  },
+  userIds: new Set(),
 
-  configure: function({ path, basicAuth, filter }) {
+  boot: function({ username, password, path, port, forward_url, refresh_frequency }) {
+    this.username = username;
+    this.password = password;
+    this.forward_url = forward_url;
     this.basicAuth = basicAuth;
-    this.filter = filter
+
+    if(this.username && this.password) {
+      this.express.use(expressBasicAuth({
+        users: { [this.username]: this.password },
+      }));
+    }
 
     this.express.post(path, (req, res) => {
       const body = JSON.parse(req.body);
@@ -25,10 +28,44 @@ let app = {
         res.status(204).end();
       }
     });
+
+    setInterval(this.refreshUserIds, refresh_frequency);
+    this.refreshUserIds().then(() => this.express.listen(port));
   },
 
-  listen: function(port) {
-    this.express.listen(port);
+  refreshUserIds: function() {
+    fetch(this.forward_url, { headers: this.authHeaders() })
+      .then(res => res.json())
+      .then(data => {
+        console.log(`Fetched ${data.length} user ids`);
+        userIds = new Set(data);
+      });
+  },
+
+  filter: function(body) {
+    const message = body["message"];
+    const userId = JSON.parse(message)["userId"];
+    const found = userIds.has(userId);
+    if(found) {
+      console.log(`Forwarding message for: ${userId}`);
+      fetch(this.forward_url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: Object.assign(
+          { 'Content-Type': 'application/json' },
+          this.authHeaders()
+        ),
+      });
+    }
+    return found;
+  },
+
+  authHeaders: function() {
+    if(this.username && this.password) {
+      return { 'Authorization': 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64') };
+    } else {
+      return {};
+    }
   },
 };
 
--- test/test.js
+++ test/test.js
@@ -9,11 +9,11 @@ describe("app", () => {
   describe("GET /", () => {
     describe("when SNS message is for a userId in the list", () => {
       it("returns 201")
-      it("sends an http request to the app")
+      it("forwards SNS message to the app")
     });
     describe("when SNS message is NOT for a userId in the list", () => {
       it("returns 204")
-      it("does not send an http request to the app")
+      it("does NOT forward SNS message to the app")
     });
   });
 });
