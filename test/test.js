8920a306490003ea75c3e6f226d1206f614014a6
Micah Geisel <micah@botandrose.com>
2020-05-14 10:49:57 -0700

    get second ported test passing.

--- test/test.js
+++ test/test.js
@@ -7,10 +7,10 @@ const app = require('../app');
 chai.use(chaiHttp);
 chai.should();
 
+let forwardUrl = "https://example.com/messages";
+
 before(async () => {
-  let forwardUrl = "https://example.com/messages";
   fetchMock.get(forwardUrl, ["1","2","3"]);
-  fetchMock.post(forwardUrl, 201);
 
   await app.boot({
     path: "/",
@@ -19,21 +19,43 @@ before(async () => {
   });
 })
 
+beforeEach(() => fetchMock.post(forwardUrl, 201));
+
+afterEach(() => fetchMock.reset());
+
+function newMessageForUserId(id) {
+  return `{"message":"{\\"timeRangeStart\\":1571245940546,\\"timeRangeEnd\\":1571246360547,\\"userId\\":\\"${id}\\",\\"appId\\":\\"com.nike.ntc.brand.ios\\",\\"sources\\":[\\"nike.ntc.ios\\"],\\"metricTypes\\":[\\"rpe\\",\\"calories\\",\\"nikefuel\\"],\\"activityId\\":\\"8ed06ba3-d12c-4dc1-a773-cba81a0f0c5c\\",\\"receivedTime\\":1571246402784,\\"changeToken\\":\\"5819147260e25392eecbb1ab10480a1ed3bc600c8a379a88a035fa163d505e44:1571246402784\\",\\"changeType\\":\\"update\\",\\"payloadType\\":\\"activity\\",\\"session\\":false,\\"activityType\\":\\"training\\"}","trace":{"parentId":"-2721724131837535344","spanId":"-8694889937887914020","spanName":"persistAndEnqueueSync","userId":"0353d6f5-f7e9-4830-a803-a1fc370bfa04","traceId":"0c967b6959f4284c","sampleable":false,"traceStartTimeNanos":1017120644185959}}`;
+}
+
 describe("app", () => {
   describe("POST /", () => {
     describe("when SNS message is for a userId in the list", () => {
-      it("returns 201", (done) => {
-        chai.request(app.express)
+      let newRequest = () => {
+        return chai.request(app.express)
           .post('/')
           .set("Content-Type", "text/plain")
-          .send('{"message":"{\\"timeRangeStart\\":1571245940546,\\"timeRangeEnd\\":1571246360547,\\"userId\\":\\"1\\",\\"appId\\":\\"com.nike.ntc.brand.ios\\",\\"sources\\":[\\"nike.ntc.ios\\"],\\"metricTypes\\":[\\"rpe\\",\\"calories\\",\\"nikefuel\\"],\\"activityId\\":\\"8ed06ba3-d12c-4dc1-a773-cba81a0f0c5c\\",\\"receivedTime\\":1571246402784,\\"changeToken\\":\\"5819147260e25392eecbb1ab10480a1ed3bc600c8a379a88a035fa163d505e44:1571246402784\\",\\"changeType\\":\\"update\\",\\"payloadType\\":\\"activity\\",\\"session\\":false,\\"activityType\\":\\"training\\"}","trace":{"parentId":"-2721724131837535344","spanId":"-8694889937887914020","spanName":"persistAndEnqueueSync","userId":"0353d6f5-f7e9-4830-a803-a1fc370bfa04","traceId":"0c967b6959f4284c","sampleable":false,"traceStartTimeNanos":1017120644185959}}')
-          .end((err, res) => {
-            res.should.have.status(201);
-            done();
-          });
+          .send(newMessageForUserId("1"));
+      };
+
+      it("returns 201", (done) => {
+        newRequest().end((err, res) => {
+          res.should.have.status(201);
+          done();
+        });
       });
-      it("forwards SNS message to the app")
+
+      it("forwards SNS message to the app", (done) => {
+        newRequest().end((err, res) => {
+          fetchMock.called(forwardUrl, {
+             method: 'POST',
+             body: JSON.parse(newMessageForUserId("1")),
+             headers: { 'Content-Type': 'application/json' },
+          }).should.be.true;
+          done();
+        });
+      });
     });
+
     describe("when SNS message is NOT for a userId in the list", () => {
       it("returns 204")
       it("does NOT forward SNS message to the app")
